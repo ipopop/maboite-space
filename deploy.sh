d40d74480bda1100caffc326d7e7310ce7b3b218
ipopop <popop@ntymail.com>
2020-09-08 13:08:30 +0200

    optim img

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,6 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
-git push -f git@github.com:ipopop/my-tailwind-project.git master:gh-pages
+git push -f git@github.com:ipopop/maboite-space.git master:gh-pages
 
 cd -
